8e3a4f2f4e28f0d2a5fa2d241f822f060258a0b1
joao vitor <joaomakenze@gmail.com>
2025-06-15 13:54:14 -0300

    adicionando a funcao de contador itens no carrrinho

--- src/pages/Profile/components/Hero/index.tsx
+++ src/pages/Profile/components/Hero/index.tsx
@@ -1,40 +1,47 @@
 import StylesHero from "./styles";
 import logo from "../../../../image/logo.svg";
 import { Link } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import type { RootState } from "../../../../store/store";
 
 type HeroProps = {
-    tipo:string;
-    titulo:string;
-    capa:string;
-}
-const Hero = ({tipo, titulo, capa} : HeroProps) => (
-    <StylesHero>
-        <header>
-            <div className="header-top">
-                <div className="header-container">
-                    <div>
-                        <h3>Restaurantes</h3>
-                    </div>
-                    <div id="container-logo">
-                        <Link id="link-logo" to="/">
-                            <img id="logo" src={logo} alt="Logo" />
-                        </Link>
-                    </div>
-                    <div id="container-cart">
-                        <p>0 produto(s) no carrinho</p>
+    tipo: string;
+    titulo: string;
+    capa: string;
+};
+
+const Hero = ({ tipo, titulo, capa }: HeroProps) => {
+    const produtos = useSelector((state: RootState) => state.cart.produtos);
+    const quantidade = produtos.length;
+
+    return (
+        <StylesHero>
+            <header>
+                <div className="header-top">
+                    <div className="header-container">
+                        <div>
+                            <h3>Restaurantes</h3>
+                        </div>
+                        <div id="container-logo">
+                            <Link id="link-logo" to="/">
+                                <img id="logo" src={logo} alt="Logo" />
+                            </Link>
+                        </div>
+                        <div id="container-cart">
+                            <p>({quantidade}) produto(s) no carrinho</p>
+                        </div>
                     </div>
                 </div>
-                
-            </div>
-            <div className="header-bottom">
-                <div className="title">
-                    <h3>{tipo}</h3>
-                    <h2>{titulo}</h2>
+                <div className="header-bottom">
+                    <div className="title">
+                        <h3>{tipo}</h3>
+                        <h2>{titulo}</h2>
+                    </div>
+                    <img src={capa} alt="banner do restaurante" />
                 </div>
-                <img src={capa} alt="banner do restaurante" />
-            </div>
-        </header>
-    </StylesHero>
-);
+            </header>
+        </StylesHero>
+    );
+};
 
 export default Hero;
